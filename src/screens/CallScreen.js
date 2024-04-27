b64a77c6c80b1a3436134d95ad2f678f3305d6a7
Dinh Xuan Phu <phuxa153@gmail.com>
2024-04-27 22:06:24 +0700

    refactor: :recycle: Remove unused code

--- src/screens/CallScreen.js
+++ src/screens/CallScreen.js
@@ -13,21 +13,8 @@ function CallScreen() {
   const socket = socketio("https://signaling-server-pfm2.onrender.com/", {
     autoConnect: false,
   });
-  // const socket = socketio("http://127.0.0.1:5004", {
-  //   autoConnect: false,
-  // });
 
   let pc; // For RTCPeerConnection Object
-
-  const sendData = (data) => {
-    // eslint-disable-next-line no-restricted-globals
-    console.log("Sending data: ", data);
-    socket.emit("data", {
-      username: localUsername,
-      room: roomName,
-      data: data,
-    });
-  };
 
   const startConnection = () => {
     try {
@@ -49,94 +36,12 @@ function CallScreen() {
     });
   };
 
-  const onIceCandidate = (event) => {
-    if (event.candidate) {
-      console.log("Sending ICE candidate");
-      sendData({
-        type: "candidate",
-        candidate: event.candidate,
-      });
-    }
-  };
-
-  const onTrack = (event) => {
-    console.log("Adding remote track");
-    remoteVideoRef.current.srcObject = event.streams[0];
-  };
-
-  const createPeerConnection = () => {
-    try {
-      pc = new RTCPeerConnection({
-        iceServers: [
-          {
-            urls: ['stun:stun1.l.google.com:19302', 'stun:stun2.l.google.com:19302'],
-          },
-          // {
-          //   urls: "turn:standard.relay.metered.ca:80",
-          //   username: "ec3ee67c7a9f58188572af0a",
-          //   credential: "hWXfsYZeTlDij524",
-          // },
-          // {
-          //   urls: "turn:standard.relay.metered.ca:80?transport=tcp",
-          //   username:"ec3ee67c7a9f58188572af0a",
-          //   credential:"hWXfsYZeTlDij524",
-          // },
-          // {
-          //   urls: "turn:standard.relay.metered.ca:443",
-          //   username: "ec3ee67c7a9f58188572af0a",
-          //   credential: "hWXfsYZeTlDij524",
-          // },
-          // {
-          //   urls: "turns:standard.relay.metered.ca:443?transport=tcp",
-          //   username: "ec3ee67c7a9f58188572af0a",
-          //   credential: "hWXfsYZeTlDij524",
-          // },
-        ],
-      });
-      pc.onicecandidate = onIceCandidate;
-      pc.ontrack = onTrack;
-      const localStream = localVideoRef.current.srcObject;
-      for (const track of localStream.getTracks()) {
-        pc.addTrack(track, localStream);
-      }
-      console.log("PeerConnection created");
-    } catch (error) {
-      console.error("PeerConnection failed: ", error);
-    }
-  };
-
-  const setAndSendLocalDescription = (sessionDescription) => {
-    console.log("Session Description: ", sessionDescription)
-    pc.setLocalDescription(sessionDescription);
-    console.log("Local description set");
-    sendData(sessionDescription);
-  };
-
-  const sendOffer = () => {
-    console.log("Sending offer");
-    pc.createOffer().then(setAndSendLocalDescription, (error) => {
-      console.error("Send offer failed: ", error);
-    });
-  };
-
-  const sendAnswer = () => {
-    console.log("Sending answer");
-    pc.createAnswer().then(setAndSendLocalDescription, (error) => {
-      console.error("Send answer failed: ", error);
-    });
-  };
 
 const signalingDataHandler = (data) => {
-  if (data.type === "offer") {
-    createPeerConnection();
-    pc.setRemoteDescription(new RTCSessionDescription(data))
-      .then(() => pc.createAnswer())
-      .then(answer => setAndSendLocalDescription(answer))
-      .catch(error => console.error("Error handling offer: ", error));
-  } else if (data.type === "answer") {
+  if (data.type === "answer") {
     console.log("pc.signalingState: ", pc.signalingState);
     if (pc.signalingState === "have-local-offer") {
-      let rtc_data = {
+      const rtc_data = {
         type: data["type"],
         sdp: data["sdp"]
       };
@@ -145,19 +50,7 @@ const signalingDataHandler = (data) => {
     } else {
       console.log("Cannot handle answer in current state: ", pc.signalingState);
     }
-  } else if (data.type === "candidate") {
-    if (pc.signalingState !== "closed" && pc.remoteDescription) {
-      try {
-        pc.addIceCandidate(new RTCIceCandidate(data.candidate));
-      } catch (error) {
-        console.error("Error adding ICE candidate: ", error);
-      }
-    } else {
-      console.log("Cannot add ICE candidate in current state: ", pc.signalingState);
-    }
-  } else {
-    console.log("Unknown Data");
-  }
+  } 
 };
 
   function negotiate() {
@@ -196,12 +89,6 @@ const signalingDataHandler = (data) => {
     });
   }
 
-  socket.on("ready", () => {
-    console.log("Ready to Connect!");
-    createPeerConnection();
-    sendOffer();
-  });
-
   socket.on("offer", (data) => {
     console.log("Offer received");
     signalingDataHandler(data);
@@ -212,11 +99,6 @@ const signalingDataHandler = (data) => {
     console.log(data);
     signalingDataHandler(data);
   });
-
-  // socket.on("data", (data) => {
-  //   console.log("Data received: ", data);
-  //   signalingDataHandler(data);
-  // });
 
   useEffect(() => {
     startConnection();
@@ -231,9 +113,6 @@ const signalingDataHandler = (data) => {
         sdpSemantics: 'unified-plan'
     };
 
-    // if (document.getElementById('use-stun').checked) {
-    //     config.iceServers = [{ urls: ['stun:stun.l.google.com:19302'] }];
-    // }
     config.iceServers = [{
       urls: ['stun:stun1.l.google.com:19302', 'stun:stun2.l.google.com:19302']
     }];
@@ -245,14 +124,9 @@ const signalingDataHandler = (data) => {
         if (evt.track.kind === 'video') {
             localVideoRef.current.srcObject = evt.streams[0];
         }
-        // else {
-        //     audioRef.current.srcObject = evt.streams[0];
-        // }
     });
 
-    // document.getElementById('start').style.display = 'none';
     negotiate();
-    // document.getElementById('stop').style.display = 'inline-block';
 }
 
 function stop() {
